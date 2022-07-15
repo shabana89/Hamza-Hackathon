9dbd36e5335fa65c0b4bbcf275eaa584701b58d5
shabana kauser <shabanakauser89@gmail.com>
2022-07-15 10:10:05 +0100

    front end and back end set up

---
+++ my-app-backend/usersData.js
@@ -0,0 +1,31 @@
+let usersData = [
+    {
+        'id': 1,
+        'firstName': 'Sally',
+        'lastName': 'Smith',
+        'age': 20
+
+    },
+    {
+        'id': 2,
+        'firstName': 'Dean',
+        'lastName': 'Watts',
+        'age': 27
+    },
+    {
+        'id': 3,
+        'firstName': 'Rachel',
+        'lastName': 'Ogden',
+        'age': 30
+    },
+    {
+        'id': 4,
+        'firstName': 'Aisha',
+        'lastName': 'Khan',
+        'age': 32
+    }
+
+
+]
+
+  export default usersData;
