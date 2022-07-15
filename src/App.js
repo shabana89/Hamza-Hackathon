aed0aa85e479e6d4e377922619c8f32dc12ccd36
shabana kauser <shabanakauser89@gmail.com>
2022-07-15 14:06:16 +0100

    front end started

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <p>{!data ? "Loading..." : data}</p>
+      <p>{!data ? "Loading......" : data}</p>
       </header>
     </div>
   );
