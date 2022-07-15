18e1ac0df3d30034a4c6885853a554123fa0f94a
shabana kauser <shabanakauser89@gmail.com>
2022-07-15 14:02:31 +0100

    added get and post

--- my-app-backend/Server/index.js
+++ my-app-backend/Server/index.js
@@ -1,14 +1,36 @@
 import express from "express";
-import usersData from './usersData.js'
+import usersData from '../usersData.js'
 
 const PORT = 3001;
-
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+  res.json({status:'ok', payload: usersData});
+});
+   
+app.get('/users/:id', (req, res) => {
+  const id = req.params.id
+  const filteredUser = usersData.filter(function(users){
+    return users.id == id
+  })
+  return res.json({filteredUser})
 });
 
+app.post('/users', (req, res) => {
+  const newUser = req.body
+  usersData.push(newUser);
+  res.json({status:'ok', payload:usersData})
+});
+
+// app.put() => {
+
+// };
+
+// app.delete() => {
+
+// };
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
